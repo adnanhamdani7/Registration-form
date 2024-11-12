6c5f2f00a681e78090d157a772b4c6040cbeae20
Adnan  Hamdani <128588520+adnanhamdani7@users.noreply.github.com>
2024-11-12 11:47:48 +0530

    Add files via upload

---
+++ registration form/script.js
@@ -0,0 +1,69 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const cnfpassword = document.getElementById("cnfpassword");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validateInputs();
+});
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const cnfPasswordValue = cnfpassword.value.trim();
+
+    if (usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if (passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 6) {
+        setError(password, 'Password must be at least 6 characters');
+    } else {
+        setSuccess(password);
+    }
+
+    if (cnfPasswordValue === '') {
+        setError(cnfpassword, 'Please confirm your password');
+    } else if (cnfPasswordValue !== passwordValue) {
+        setError(cnfpassword, 'Passwords do not match');
+    } else {
+        setSuccess(cnfpassword);
+    }
+};
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+};
+
+const setSuccess = (element) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+};
+
